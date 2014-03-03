68992b837242c649b1d77d69be2f5e51cd078187
Stewart Smith <stewart@linux.vnet.ibm.com>
2014-03-03 16:59:54 +1100

    Remove unneeded opal_errd.h, moving defines into opal_errd.c
    Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>
    Acked-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- opal_errd/opal_errd.c
+++ opal_errd/opal_errd.c
@@ -24,8 +24,37 @@
 #include <signal.h>
 #include <sys/stat.h>
 
-#include "opal_errd.h"
 #include "platform.c"
+
+#define SYSFS_ELOG	"/sys/firmware/opal/opal_elog"
+#define SYSFS_ELOG_ACK	"/sys/firmware/opal/opal_elog_ack"
+#define PLATFORM_LOG	"/var/log/platform"
+
+#define EXTRACT_OPAL_DUMP_CMD	"/usr/sbin/extract_opal_dump"
+
+/*
+ * As per PEL v6 (defined in PAPR spec) fixed offset for
+ * error log information.
+ */
+#define OPAL_ERROR_LOG_MAX	16384
+#define ELOG_ID_SIZE		4
+#define ELOG_SRC_SIZE		8
+
+#define ELOG_ID_OFFESET		0x2c
+#define ELOG_DATE_OFFSET	0x8
+#define ELOG_TIME_OFFSET	0xc
+#define ELOG_SRC_OFFSET		0x78
+#define ELOG_SEVERITY_OFFSET	0x3a
+#define ELOG_SUBSYSTEM_OFFSET	0x38
+#define ELOG_ACTION_OFFSET	0x42
+
+/* Severity of the log */
+#define OPAL_INFORMATION_LOG	0x00
+#define OPAL_RECOVERABLE_LOG	0x10
+#define OPAL_PREDICTIVE_LOG	0x20
+#define OPAL_UNRECOVERABLE_LOG	0x40
+
+#define ELOG_ACTION_FLAG	0xa8000000
 
 static int sysfs_elog_fd = -1;
 static int sysfs_elog_id_fd = -1;

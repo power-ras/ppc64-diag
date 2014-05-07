1bbaed1d81c16e5df3f3029ef056c3be19ee22b5
Cyril Bur <cyril@ozlabs.au.ibm.com>
2014-05-07 16:38:07 +1000

    opal-elog-parse: add more helpers to opal-event-log
    Save putting data structure searching/writing code throughout the project.
    
    Signed-off-by: Cyril Bur <cyril@ozlabs.au.ibm.com>

--- opal_errd/opal-elog-parse/opal-event-log.c
+++ opal_errd/opal-elog-parse/opal-event-log.c
@@ -1,6 +1,31 @@
 #include <stdlib.h>
+#include <errno.h>
 #include <string.h>
 #include "opal-event-log.h"
+
+opal_event_log *create_opal_event_log(int n) {
+	opal_event_log *log = (struct opal_event_log_scn *)
+		malloc(sizeof(struct opal_event_log_scn) * (n + 1));
+	if (!log)
+		return NULL;
+
+	log[n].id[0] = '\0';
+	log[n].id[1] = '\0';
+	log[n].scn = NULL;
+
+	return log;
+}
+
+
+int add_opal_event_log_scn(opal_event_log *log, const char *id, void *scn, int n) {
+	if(!log || n < 0)
+		return -EINVAL;
+
+	memcpy(log[n].id, id, 2);
+	log[n].scn = scn;
+
+	return 0;
+}
 
 int has_more_elements(struct opal_event_log_scn log_scn) {
    return !(log_scn.id[0] == '\0' && log_scn.id[1] == '\0' && log_scn.scn == NULL);
@@ -18,4 +43,20 @@ void *get_opal_event_log_scn(opal_event_log *log, const char *id, int n) {
    }
 
    return NULL;
+}
+
+struct opal_event_log_scn *
+get_nth_opal_event_log_scn(opal_event_log log, int n)
+{
+	if (n < 0)
+		return NULL;
+
+	int i = 0;
+	while (has_more_elements(log[i])) {
+		if(n-- == 0)
+			return log + i;
+		i++;
+	}
+
+	return NULL;
 }
--- opal_errd/opal-elog-parse/opal-event-log.h
+++ opal_errd/opal-elog-parse/opal-event-log.h
@@ -8,6 +8,15 @@ struct opal_event_log_scn {
 
 typedef struct opal_event_log_scn opal_event_log;
 
+opal_event_log *create_opal_event_log(int n);
+
+int add_opal_event_log_scn(opal_event_log *log, const char *id, void *scn, int n);
+
 int has_more_elements(struct opal_event_log_scn log_scn);
+
 void *get_opal_event_log_scn(opal_event_log *log, const char *id, int n);
+
+void *get_opal_event_log_scn(opal_event_log *log, const char *id, int n);
+
+
 #endif /* _H_OPAL_EVENT_LOG */

b070cbe5f302d491a3ae7982e89ce14594ac89e2
Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
2015-01-23 14:48:24 +0530

    v6ela.c : Cleaup trailing whitespace
    Cleanup trailing whitespace and tabs
    
    Signed-off-by: Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
    Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>

--- rtas_errd/v6ela.c
+++ rtas_errd/v6ela.c
@@ -19,14 +19,14 @@
 #define V6_EVENT_MSG	1
 
 #define NUM_SUBID	12
-#define NUM_SEVERITY	14 
-#define NUM_SUBTYPE	14 
+#define NUM_SEVERITY	14
+#define NUM_SUBTYPE	14
 
 /* 
  * Table of V6 Error Messages. Each message, V6_ER_ss_vv,  is addressable 
  * by the Subsystem Id ("ss") and the Error Severity ("vv"). 
  */
-char *v6_error[NUM_SUBID][NUM_SEVERITY] = { 
+char *v6_error[NUM_SUBID][NUM_SEVERITY] = {
 	{V6_ER_10_00, V6_ER_10_10, V6_ER_10_20, V6_ER_10_21, V6_ER_10_22,
 	 V6_ER_10_23, V6_ER_10_24, V6_ER_10_40, V6_ER_10_41, V6_ER_10_44,
 	 V6_ER_10_45, V6_ER_10_48, V6_ER_10_60, V6_ER_10_61},
@@ -145,11 +145,11 @@ char *
 get_message_id(int type, struct rtas_usr_hdr_scn *usrhdr)
 {
 	int i,j,k;
-	int supported_severity[NUM_SEVERITY] = { 0, 0x10, 0x20, 0x21, 0x22, 
+	int supported_severity[NUM_SEVERITY] = { 0, 0x10, 0x20, 0x21, 0x22,
 	                                         0x23, 0x24, 0x40, 0x41, 0x44,
-	                                         0x45, 0x48, 0x60, 0x61}; 
-	int supported_subtype[NUM_SUBTYPE]   = { 0, 0x01, 0x08, 0x10, 0x20, 
-	                                         0x21, 0x22, 0x30, 0x40, 0x60, 
+	                                         0x45, 0x48, 0x60, 0x61};
+	int supported_subtype[NUM_SUBTYPE]   = { 0, 0x01, 0x08, 0x10, 0x20,
+	                                         0x21, 0x22, 0x30, 0x40, 0x60,
 	                                         0x70, 0x80, 0xD0, 0xE0};
 
 	/* Subsystem IDs start at 0x10 according to the PAPR.
@@ -158,15 +158,15 @@ get_message_id(int type, struct rtas_usr_hdr_scn *usrhdr)
 		return (V6_INVALID_SUBID);
 
 	/* Subsystem IDs in the range 0xB0 to 0xFF are reserved */
-	if (usrhdr->subsystem_id > 0xAF) 
+	if (usrhdr->subsystem_id > 0xAF)
 		return (V6_RESERVED_SUBID);
 
 	/* Use upper 4 bits for message index */
 	i = usrhdr->subsystem_id >> 4;
-	
-	if (usrhdr->subsystem_id > 0x79) 
+
+	if (usrhdr->subsystem_id > 0x79)
 		i++;
-	
+
 	/* Decrement subsystem ID index into message arrays
 	   (index 0 = message for subsystem ID 0x10 - 0x1F, etc) */
 	i--;
@@ -179,10 +179,10 @@ get_message_id(int type, struct rtas_usr_hdr_scn *usrhdr)
 			j = 0;
 
 		return (v6_error[i][j]);
-	} 
+	}
 	else {
 		/* V6_EVENT_MSG */
-		for (k = 0, j = -1; k < NUM_SUBTYPE && j == -1; k++) 
+		for (k = 0, j = -1; k < NUM_SUBTYPE && j == -1; k++)
 			if (supported_subtype[k] == usrhdr->event_type)
 				j = k;
 		if (j == -1)
@@ -258,7 +258,7 @@ report_src(struct event *event, struct rtas_priv_hdr_scn *privhdr,
 		if (fruid_has_part_no(fru_id)) {
 			strncpy(frupn, fru_id->part_no, 20);
 
-			if (fruid_has_ccin(fru_id)) 
+			if (fruid_has_ccin(fru_id))
 				strncpy(ccin, fru_id->ccin, 20);
 
 			if (fruid_has_serial_no(fru_id))
@@ -266,7 +266,7 @@ report_src(struct event *event, struct rtas_priv_hdr_scn *privhdr,
 		}
 
 		if (fruid_has_proc_id(fru_id))
-			strncpy(proc_id, fru_id->procedure_id, 20); 
+			strncpy(proc_id, fru_id->procedure_id, 20);
 
 		add_callout(event, fru->priority, type, proc_id, loccode,
 			    frupn, frusn, ccin);
@@ -291,7 +291,7 @@ report_src(struct event *event, struct rtas_priv_hdr_scn *privhdr,
  *
  */
 #define  DISPLAY_SRC_SIZE        8      /* Display 8 digits of the SRC */
-static void 
+static void
 report_menugoal(struct event *event, struct rtas_priv_hdr_scn *privhdr,
 		struct rtas_usr_hdr_scn *usrhdr)
 {
@@ -300,16 +300,16 @@ report_menugoal(struct event *event, struct rtas_priv_hdr_scn *privhdr,
 	int offset = 0;
 	uint menu_num = 0;
         long time_loc;
-        struct tm *date = NULL; 
+        struct tm *date = NULL;
 
 	memset(buffer, 0, sizeof(buffer));
-	
+
 	/* Create the menugoal text */
 
 	if (usrhdr->event_severity) {
 		struct rtas_epow_scn *epow;
 		struct rtas_src_scn *src;
-	
+
 		epow = rtas_get_epow_scn(event->rtas_event);
 
 		/* This menu goal is a result of V6 log marked as an error, */
@@ -351,13 +351,13 @@ report_menugoal(struct event *event, struct rtas_priv_hdr_scn *privhdr,
 					menu_num = 651161;
 				break;
 			}
-			
+
 			if (date != NULL)
-				offset += sprintf(buffer + offset, msg, 
-						  asctime(date)); 
+				offset += sprintf(buffer + offset, msg,
+						  asctime(date));
 			else
 				offset += sprintf(buffer + offset, "%s", msg);
-		} 
+		}
 
 		if (menu_num == 0) {
 			/* Not resolved by EPOW section */
@@ -372,16 +372,16 @@ report_menugoal(struct event *event, struct rtas_priv_hdr_scn *privhdr,
 		if (src != NULL && strlen(src->primary_refcode)) {
 			struct rtas_fru_scn *fru;
 			char tmp_buf[MAX_MENUGOAL_SIZE/2];
-			char tmp_src[DISPLAY_SRC_SIZE+1]; 
+			char tmp_src[DISPLAY_SRC_SIZE+1];
 			int tmp_off = 0;
 
 			memset(tmp_buf, 0, sizeof(tmp_buf));
-			
+
 			strncpy(tmp_src, src->primary_refcode, 8);
 			tmp_src[DISPLAY_SRC_SIZE] = '\0';
-			offset += sprintf(buffer + offset, 
+			offset += sprintf(buffer + offset,
 					  MSGMENUG_SRC, tmp_src);
-			
+
 			for (fru = src->fru_scns; fru != NULL; fru = fru->next){
 				struct rtas_fru_hdr *fruhdr = fru->subscns;
 				struct rtas_fru_id_scn *fru_id;
@@ -402,11 +402,11 @@ report_menugoal(struct event *event, struct rtas_priv_hdr_scn *privhdr,
 					case RTAS_FRUID_COMP_CONFIG_ERROR:
 						fru_msg = MSGMENUG_CFG;
 						break;
-					case RTAS_FRUID_COMP_MAINT_REQUIRED: 
+					case RTAS_FRUID_COMP_MAINT_REQUIRED:
 						fru_msg = MSGMENUG_MAINT;
 						break;
 					case RTAS_FRUID_COMP_EXTERNAL:
-						fru_msg = MSGMENUG_EXT_FRU; 
+						fru_msg = MSGMENUG_EXT_FRU;
 						break;
 					case RTAS_FRUID_COMP_EXTERNAL_CODE:
 						fru_msg = MSGMENUG_EXT_CODEFRU;
@@ -422,39 +422,39 @@ report_menugoal(struct event *event, struct rtas_priv_hdr_scn *privhdr,
 				}
 
 				if (fruid_has_part_no(fru_id)) {
-				    	tmp_off += sprintf(tmp_buf + tmp_off, 
-							   fru_msg, 
+					tmp_off += sprintf(tmp_buf + tmp_off,
+							   fru_msg,
 							   fru_id->part_no);
 
 					if (fruid_has_ccin(fru_id)) {
-						tmp_off += 
-						    sprintf(tmp_buf + tmp_off, 
+						tmp_off +=
+						    sprintf(tmp_buf + tmp_off,
 							    MSGMENUG_CCIN,
 							    fru_id->ccin);
 					}
-					
+
 					if (fruid_has_serial_no(fru_id)) {
-						tmp_off += 
-						    sprintf(tmp_buf + tmp_off, 
-							    MSGMENUG_SERIAL, 
+						tmp_off +=
+						    sprintf(tmp_buf + tmp_off,
+							    MSGMENUG_SERIAL,
 							    fru_id->serial_no);
 					}
 				} else if (fruid_has_proc_id(fru_id))
-				    	tmp_off += sprintf(tmp_buf + tmp_off, 
+					tmp_off += sprintf(tmp_buf + tmp_off,
 							  fru_msg,
 							  fru_id->procedure_id);
 
 				/* Add location code if present */
 				if (fru->loc_code_length) {
-				    	tmp_off += sprintf(tmp_buf + tmp_off, 
+					tmp_off += sprintf(tmp_buf + tmp_off,
 							   MSGMENUG_LOCATION,
 							   fru->loc_code);
 				} else
-					tmp_off += sprintf(tmp_buf + tmp_off, 
+					tmp_off += sprintf(tmp_buf + tmp_off,
 							   "\n");
 
-				offset += sprintf(buffer + offset, 
-						  MSGMENUG_PRIORITY, 
+				offset += sprintf(buffer + offset,
+						  MSGMENUG_PRIORITY,
 						  fru->priority, tmp_buf);
 
 			}
@@ -464,9 +464,9 @@ report_menugoal(struct event *event, struct rtas_priv_hdr_scn *privhdr,
 		/* so don't include any SRC or FRU call outs because it     */
 		/* will appear like an error and cause customer concern.    */
 		menu_num = 651301;
-		msg = get_message_id(V6_EVENT_MSG, usrhdr); 
+		msg = get_message_id(V6_EVENT_MSG, usrhdr);
 		offset += sprintf(buffer + offset, "%s%s\n\n",
-				  MSGMENUGPEL_EVENT, msg); 
+				  MSGMENUGPEL_EVENT, msg);
 	}
 
 	event->sl_entry->call_home_status = SL_CALLHOME_NONE;
@@ -490,7 +490,7 @@ report_menugoal(struct event *event, struct rtas_priv_hdr_scn *privhdr,
  * @param event the event to be processed
  * @return 0 on success; !0 on failure
  */
-int 
+int
 process_v6(struct event *event)
 {
 	struct rtas_event_exthdr *exthdr;

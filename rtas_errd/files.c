power-ras/ppc64-diag

commit 81f61aad84bceac26ad6103803910f7c8e7d4e70

rtas_errd/files: Replace system() with native C functions
Replace the occurrence of system() calls with native
unlink()/rename().

Signed-off-by: Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>

rtas_errd/files.c
@@ -14,6 +14,7 @@
 #include <stdarg.h>
 #include <string.h>
 #include <ctype.h>
+#include <libgen.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <sys/wait.h>
@@ -580,21 +581,45 @@ _log_msg(struct event *event, const char *fmt, va_list ap)
 	}
 
 	if (sbuf.st_size >= RTAS_ERRD_LOGSZ) {
-		char	cmd_buf[1024];
-		int	rc;
-		int8_t	status;
+		int rc;			/* return code */
+		struct stat log_file;	/* file stat   */
+		int dir_fd;		/* fsync       */
 
 		close(rtas_errd_log_fd);
 
-		memset(cmd_buf, 0, 1024);
-		sprintf(cmd_buf, "rm -f %s; mv %s %s", rtas_errd_log0,
-			rtas_errd_log, rtas_errd_log0);
-		rc = system(cmd_buf);
+		/*
+		 * Remove log file /var/log/rtas_errd.log0
+		 */
+		rc = stat(rtas_errd_log0, &log_file);
+		if (rc == 0) {
+			rc = unlink(rtas_errd_log0);
+			if (rc == -1) {
+				log_msg(NULL, "An error occured during the "
+					"rotation of the rtas_errd logs:\n "
+					"cmd = rm %s\nexit status: %d)",
+					rtas_errd_log0, errno);
+			}
+		} /* stat */
+
+		/*
+		 * rotate /var/log/rtas_errd.log to /var/log/rtas_errd.log0
+		 */
+		rc = rename(rtas_errd_log, rtas_errd_log0);
 		if (rc == -1) {
-			status = WEXITSTATUS(rc);
-			log_msg(NULL, "An error occured during the rotation of "
-				"the rtas_errd logs:\n cmd = %s\nexit status: "
-				"%d)", cmd_buf, status);
+			log_msg(NULL, "An error occured during rotation of "
+				"rtas_errd logs (logger alive):\n cmd = rename"
+				" %s %s\nexit status: %d)", rtas_errd_log,
+				rtas_errd_log0, errno);
+		}
+
+		/*
+		 * fsync /var/log/ to be  on safer side.
+		 */
+		dir_fd = open(dirname(rtas_errd_log), O_RDONLY|O_DIRECTORY);
+		rc = fsync(dir_fd);
+		if (rc == -1) {
+			log_msg(NULL, "fsync failed, on %s\nexit status: %d",
+					dirname(rtas_errd_log), errno);
 		}
 
 		rtas_errd_log_fd = open(rtas_errd_log,

commit 04670ee3f6f1b04a81cf83adc3b77e162bd52b3e

rtas_errd/files: Fix dirname clobber
Commit 81f61aad8 introduced a segfault with dirname(). This patch
passes the copy of rtas_err_log file name, as it might get clobbered.

Signed-off-by: Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>

rtas_errd/files.c
@@ -512,7 +512,7 @@ reformat_msg(char *msg)
 /**
  * _log_msg
  * @brief The real routine to write messages to rtas_errd_log
- * 
+ *
  * This is a common routine for formatting messages that go to the
  * /var/log/rtas_errd.log file.  Users should pass in a reference to
  * the rtas_event structure if this message is directly related a rtas
@@ -522,11 +522,11 @@ reformat_msg(char *msg)
  *
  * This routine will do several things to the message before printing
  * it out;
- * - Add a timestamp 
+ * - Add a timestamp
  * - If a rtas_event reference is passed in, a sequenbce number is added
  * - If errno is set, the results of perror are added.
- * - The entire message is then formatted to fit in 80 cols. 
- * 
+ * - The entire message is then formatted to fit in 80 cols.
+ *
  * @param event reference to event
  * @param fmt formatted string a la printf()
  * @param ... additional args a la printf()
@@ -537,6 +537,9 @@ _log_msg(struct event *event, const char *fmt, va_list ap)
 	struct stat sbuf;
 	char	buf[RTAS_ERROR_LOG_MAX];
 	int	len = 0, rc;
+	char	*dir_name;
+        char	*rtas_errd_log_c; /* Both rtas_errd_log/rtas_errd_log0 are
+				     global, so make a copy. */
 
 	if (rtas_errd_log_fd == -1) {
 		dbg("rtas_errd log file is not available");
@@ -622,11 +625,21 @@ _log_msg(struct event *event, const char *fmt, va_list ap)
 		/*
 		 * fsync /var/log/ to be  on safer side.
 		 */
-		dir_fd = open(dirname(rtas_errd_log), O_RDONLY|O_DIRECTORY);
+		rtas_errd_log_c = strdup(rtas_errd_log);
+		if (!rtas_errd_log_c) {
+			log_msg(NULL, "%s, Memory allocation failed, %s",
+					__func__, strerror(errno));
+			rtas_errd_log_fd = -1;
+			dbg("Could not re-open %s", rtas_errd_log);
+			return;
+
+		}
+		dir_name = dirname(rtas_errd_log_c);
+		dir_fd = open(dir_name, O_RDONLY|O_DIRECTORY);
 		rc = fsync(dir_fd);
 		if (rc == -1) {
 			log_msg(NULL, "fsync failed, on %s\nexit status: %d",
-					dirname(rtas_errd_log), errno);
+					dir_name, errno);
 		}
 		close(dir_fd);
 
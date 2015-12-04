power-ras/ppc64-diag

commit ee3e3311fa4efdb37de44b7459e2db7bb717efaa

rtas_errd/diag_support: get_dt_status() use mkstemp
Use mkstemp() to create temp files, instead of hardcoding
temp files. As it's unsafe on systems  without symlink and
hardlink restrictions

Signed-off-by: Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
Cc: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>

rtas_errd/diag_support.c
@@ -233,7 +233,8 @@ get_dt_status(char *dev)
 	char target[80];
 	char *ptr;
 	char *system_args[6] = {NULL, };		/* execv args		*/
-	char *tmp_file = "/tmp/get_dt_files";
+	char tmp_file[] = "/tmp/get_dt_files";
+	int fd;
 	pid_t cpid;					/* child pid		*/
 	int rc;						/* return value		*/
 	int status;					/* child exit status	*/
@@ -244,8 +245,16 @@ get_dt_status(char *dev)
 	system_args[3] = "status";
 	system_args[4] = "-print";
 
+	fd = mkstemp(tmp_file);
+	if (fd == -1) {
+		log_msg(NULL, "tmp file creation failed, at "
+			"get_dt_status\n");
+		exit (-2);
+	} /* open */
+
 	cpid = fork();
 	if (cpid == -1) {
+		close(fd);
 		log_msg(NULL, "Fork failed, at get_dt_status\n");
 		return NULL;
 	} /* fork */
@@ -272,6 +281,7 @@ get_dt_status(char *dev)
 		close(fd);
 		exit (-2);
 	} else { /* parent */
+		close(fd);
 		rc = waitpid(cpid, &status, 0);
 		if (rc == -1) {
 			log_msg(NULL, "wait on child failed at, "
@@ -285,14 +295,14 @@ get_dt_status(char *dev)
 	}
 
 	/* results of the find command */
-	fp1 = fopen("/tmp/get_dt_files", "r");
+	fp1 = fopen(tmp_file, "r");
 	if (fp1 == 0) {
-		fprintf(stderr, "open failed on /tmp/get_dt_files\n");
+		fprintf(stderr, "open failed on %s\n", tmp_file);
 		return NULL;
 	}
 
 	while (fscanf (fp1, "%s", loc_file) != EOF) {
-		dbg("read from /tmp/get_dt_files, \"%s\"", loc_file);
+		dbg("read from /%s, \"%s\"", tmp_file, loc_file);
 
 		/* read the status in case this is the one */
 		fp2 = fopen(loc_file, "r");

commit d9899e405d2d364de8a45475d421f65efeb728c8

rtas_errd: Fix temp filename passed to mkstemp
The last six characters of template must be "XXXXXX" and these are
replaced with a string that makes the filename unique.

Fixes: ee3e3311 (rtas_errd/diag_support: get_dt_status() use mkstemp)
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
CC: Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
Reviewed-by: Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>

rtas_errd/diag_support.c
@@ -233,7 +233,7 @@ get_dt_status(char *dev)
 	char target[80];
 	char *ptr;
 	char *system_args[6] = {NULL, };		/* execv args		*/
-	char tmp_file[] = "/tmp/get_dt_files";
+	char tmp_file[] = "/tmp/get_dt_files-XXXXXX";
 	int fd;
 	pid_t cpid;					/* child pid		*/
 	int rc;						/* return value		*/
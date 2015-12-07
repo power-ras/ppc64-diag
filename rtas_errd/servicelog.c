power-ras/ppc64-diag

commit d9eddb2e2fc73d59dd2513803936abb0e7804d16

rtas_errd: Remove cast from malloc() + add error check
Casting malloc()/calloc() hide errors, remove them and also
check for memory allocation failures.

Signed-off-by: Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
Cc: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>

rtas_errd/servicelog.c
@@ -127,44 +127,70 @@ add_callout(struct event *event, char pri, int type, char *proc, char *loc,
 	struct sl_callout *callout = event->sl_entry->callouts;
 
 	if (!callout) {
-		event->sl_entry->callouts = (struct sl_callout *)malloc(
-							sizeof(*callout));
+		event->sl_entry->callouts = malloc(sizeof(*callout));
 		callout = event->sl_entry->callouts;
 	}
 	else {
 		while (callout->next)
 			callout = callout->next;
-		callout->next = (struct sl_callout *)malloc(sizeof(*callout));
+		callout->next = malloc(sizeof(*callout));
 		callout = callout->next;
 	}
 
-	if (!callout) return;
+	if (!callout)
+		goto mem_fail;
 
 	memset(callout, 0, sizeof(*callout));
 
 	callout->priority = pri;
 	callout->type = type;
 	if (proc) {
-		callout->procedure = (char *)malloc(strlen(proc) + 1);
+		callout->procedure = malloc(strlen(proc) + 1);
+		if (callout->procedure == NULL)
+			goto mem_fail;
 		strncpy(callout->procedure, proc, strlen(proc) + 1);
 	}
 	if (loc) {
-		callout->location = (char *)malloc(strlen(loc) + 1);
+		callout->location = malloc(strlen(loc) + 1);
+		if (callout->location == NULL)
+			goto mem_fail;
 		strncpy(callout->location, loc, strlen(loc) + 1);
 	}
 	if (pn) {
-		callout->fru = (char *)malloc(strlen(pn) + 1);
+		callout->fru = malloc(strlen(pn) + 1);
+		if (callout->fru == NULL)
+			goto mem_fail;
 		strncpy(callout->fru, pn, strlen(pn) + 1);
 	}
 	if (sn) {
-		callout->serial = (char *)malloc(strlen(sn) + 1);
+		callout->serial = malloc(strlen(sn) + 1);
+		if (callout->serial == NULL)
+			goto mem_fail;
 		strncpy(callout->serial, sn, strlen(sn) + 1);
 	}
 	if (ccin) {
-		callout->ccin = (char *)malloc(strlen(ccin) + 1);
+		callout->ccin = malloc(strlen(ccin) + 1);
+		if (callout->ccin == NULL)
+			goto mem_fail;
 		strncpy(callout->ccin, ccin, strlen(ccin) + 1);
 	}
 
+	return;
+
+mem_fail:
+	if (callout->ccin)
+		free(callout->ccin);
+	if (callout->serial)
+		free(callout->serial);
+	if (callout->fru)
+		free(callout->fru);
+	if (callout->location)
+		free(callout->location);
+	if (callout->procedure)
+		free(callout->procedure);
+	if (callout)
+		free(callout);
+	log_msg(event, "Memory allocation failed");
 	return;
 }
 
@@ -218,7 +244,11 @@ log_event(struct event *event)
 
 			new_len = strlen(event->sl_entry->description) +
 				  txtlen + 3;
-			new_desc = (char *)malloc(new_len);
+			new_desc = malloc(new_len);
+			if (new_desc == NULL) {
+				log_msg(event, "Memory allocation failed");
+				return;
+			}
 			snprintf(new_desc, new_len, "%s\n\n%s",
 				 event->sl_entry->description,
 				 event->addl_text);
@@ -226,7 +256,11 @@ log_event(struct event *event)
 			free(event->sl_entry->description);
 			event->sl_entry->description = new_desc;
 		} else {
-			event->sl_entry->description = (char *)malloc(txtlen+1);
+			event->sl_entry->description = malloc(txtlen+1);
+			if (event->sl_entry->description == NULL) {
+				log_msg(event, "Memory allocation failed");
+				return;
+			}
 			snprintf(event->sl_entry->description, txtlen,
 				 "%s", event->addl_text);
 		}

rtas_errd/diag_support.c
@@ -121,7 +121,7 @@ lsvpd_read(struct event *event, FILE *fp)
 			line[strlen(line) - 1] = '\0';
 
 		if (! strncmp(line, "*DS", 3)) {
-			event->diag_vpd.ds = (char *)malloc(strlen (line) + 1);
+			event->diag_vpd.ds = malloc(strlen (line) + 1);
 			if (event->diag_vpd.ds == NULL)
 				return rc;
 
@@ -130,7 +130,7 @@ lsvpd_read(struct event *event, FILE *fp)
 		}
 
 		if (! strncmp(line, "*YL", 3)) {
-			event->diag_vpd.yl = (char *)malloc(strlen (line) + 1);
+			event->diag_vpd.yl = malloc(strlen (line) + 1);
 			if (event->diag_vpd.yl == NULL)
 				return rc;
 
@@ -139,7 +139,7 @@ lsvpd_read(struct event *event, FILE *fp)
 		}
 
 		if (! strncmp(line, "*FN", 3)) {
-			event->diag_vpd.fn = (char *)malloc(strlen (line) + 1);
+			event->diag_vpd.fn = malloc(strlen (line) + 1);
 			if (event->diag_vpd.fn == NULL)
 				return rc;
 
@@ -148,7 +148,7 @@ lsvpd_read(struct event *event, FILE *fp)
 		}
 
 		if (! strncmp(line, "*SN", 3)) {
-			event->diag_vpd.sn = (char *)malloc(strlen (line) + 1);
+			event->diag_vpd.sn = malloc(strlen (line) + 1);
 			if (event->diag_vpd.sn == NULL)
 				return rc;
 
@@ -157,7 +157,7 @@ lsvpd_read(struct event *event, FILE *fp)
 		}
 
 		if (! strncmp(line, "*SE", 3)) {
-			event->diag_vpd.se = (char *)malloc(strlen (line) + 1);
+			event->diag_vpd.se = malloc(strlen (line) + 1);
 			if (event->diag_vpd.se == NULL)
 				return rc;
 
@@ -166,7 +166,7 @@ lsvpd_read(struct event *event, FILE *fp)
 		}
 
 		if (! strncmp(line, "*TM", 3)) {
-			event->diag_vpd.tm = (char *)malloc(strlen (line) + 1);
+			event->diag_vpd.tm = malloc(strlen (line) + 1);
 			if (event->diag_vpd.tm == NULL)
 				return rc;
 

rtas_errd/ela.c
@@ -428,7 +428,7 @@ save_epow_reset(struct event *event, int error_type)
 {
 	struct epow_reset *new_reset;
 
-	new_reset = (struct epow_reset *)malloc(sizeof(*new_reset));
+	new_reset = malloc(sizeof(*new_reset));
 	if (new_reset == NULL)
 		return -1;
 
@@ -565,7 +565,11 @@ process_pre_v6(struct event *event)
 	}
 
 	/* create and populate an entry to be logged to servicelog */
-	event->sl_entry = (struct sl_event *)malloc(sizeof(struct sl_event));
+	event->sl_entry = malloc(sizeof(struct sl_event));
+	if (event->sl_entry == NULL) {
+		log_msg(event, "Memory allocation failed");
+		return -1;
+	}
 	memset(event->sl_entry, 0, sizeof(struct sl_event));
 	event->sl_entry->time_event = get_event_date(event);
 	event->sl_entry->type = SL_TYPE_BASIC;
@@ -586,7 +590,12 @@ process_pre_v6(struct event *event)
 	}
 
 	event->sl_entry->raw_data_len = event->length;
-	event->sl_entry->raw_data = (unsigned char *)malloc(event->length);
+	event->sl_entry->raw_data = malloc(event->length);
+	if (event->sl_entry->raw_data == NULL) {
+		free(event->sl_entry);
+		log_msg(event, "Memory allocation failed");
+		return -1;
+	}
 	memcpy(event->sl_entry->raw_data, event->event_buf, event->length);
 
 	deconfig_error = exthdr->unrecoverable_bypassed;
@@ -1320,7 +1329,11 @@ report_srn(struct event *event, int nlocs,
 
 	event->sl_entry->call_home_status = SL_CALLHOME_CANDIDATE;
 
-	event->sl_entry->description = (char *)malloc(strlen(e_desc->rmsg)+1);
+	event->sl_entry->description = malloc(strlen(e_desc->rmsg)+1);
+	if (event->sl_entry->description == NULL) {
+		log_msg(event, "Memory allocation failed");
+		return -1;
+	}
 	strcpy(event->sl_entry->description, e_desc->rmsg);
 
 	dbg("srn: \"%s\"", event->sl_entry->refcode);
@@ -1372,7 +1385,7 @@ get_loc_code(struct event *event, int mode, int *nlocs)
 			copyLCB = NULL;
 		}
 
-		copyLCB = (char *)malloc(strlen(event->loc_codes) + 1);
+		copyLCB = malloc(strlen(event->loc_codes) + 1);
 		if (copyLCB == NULL)
 			return NULL;
 
@@ -1545,12 +1558,21 @@ report_menugoal(struct event *event, char *fmt, ...)
 		}
 
 		snprintf(menu_num_str, 20, "#%d", atoi(buffer));
-		event->sl_entry->refcode = (char *)malloc(
+		event->sl_entry->refcode = malloc(
 						strlen(menu_num_str)+1);
+		if (event->sl_entry->refcode == NULL) {
+			log_msg(event, "Memory allocation failed.\n");
+			return -1;
+		}
 		strcpy(event->sl_entry->refcode, menu_num_str);
 
 		msg = strchr(buffer, ' ') + 1;
-		event->sl_entry->description = (char *)malloc(strlen(msg)+1);
+		event->sl_entry->description = malloc(strlen(msg)+1);
+		if (event->sl_entry->description == NULL) {
+			free(event->sl_entry->refcode);
+			log_msg(event, "Memory allocation failed.\n");
+			return -1;
+		}
 		strcpy(event->sl_entry->description, msg);
 
 		dbg("menugoal: number = %s, message = \"%s\"", menu_num_str,
@@ -1704,7 +1726,7 @@ get_cpu_frus(struct event *event)
 	int nlocs = 0;
 	int rc = RC_INVALID;
 
-	buf = (char *)malloc(strlen(event->loc_codes)+4);
+	buf = malloc(strlen(event->loc_codes)+4);
 	if (buf == NULL)
 		return 0;
 
@@ -1743,7 +1765,7 @@ get_cpu_frus(struct event *event)
 				/* Rearrange locs as loc2, loc1 */
 				if (event->loc_codes != NULL)
 					free(event->loc_codes);
-				event->loc_codes = (char *)
+				event->loc_codes =
 					malloc(strlen(loc1) +
 						     strlen(loc2) + 2);
 				if (event->loc_codes == NULL)
@@ -1756,7 +1778,7 @@ get_cpu_frus(struct event *event)
 				/* Rearrange loc as loc2, loc1, loc3 */
 				if (event->loc_codes != NULL)
 					free(event->loc_codes);
-				event->loc_codes = (char *)
+				event->loc_codes =
 					malloc(strlen(loc1) +
 						     strlen(loc2) +
 						     strlen(loc3) + 3);

rtas_errd/v6ela.c
@@ -215,12 +215,21 @@ report_src(struct event *event, struct rtas_priv_hdr_scn *privhdr,
 		return 1;
 	}
 
-	event->sl_entry->refcode = (char *)malloc(strlen(src->primary_refcode)
+	event->sl_entry->refcode = malloc(strlen(src->primary_refcode)
 						  +1);
+	if (event->sl_entry->refcode == NULL) {
+		log_msg(event, "Memory allocation failed\n");
+		return 1;
+	}
 	strcpy(event->sl_entry->refcode, src->primary_refcode);
 
 	msg = get_message_id(V6_ERROR_MSG, usrhdr);
-	event->sl_entry->description = (char *)malloc(strlen(msg)+1);
+	event->sl_entry->description = malloc(strlen(msg)+1);
+	if (event->sl_entry->description == NULL) {
+		free(event->sl_entry->refcode);
+		log_msg(event, "Memory allocation failed\n");
+		return 1;
+	}
 	strcpy(event->sl_entry->description, msg);
 
 	rtas_data->addl_words[0] = src->ext_refcode2;
@@ -472,10 +481,19 @@ report_menugoal(struct event *event, struct rtas_priv_hdr_scn *privhdr,
 	event->sl_entry->call_home_status = SL_CALLHOME_NONE;
 
 	snprintf(menu_num_str, 20, "#%d", menu_num);
-	event->sl_entry->refcode = (char *)malloc(strlen(menu_num_str)+1);
+	event->sl_entry->refcode = malloc(strlen(menu_num_str)+1);
+	if (event->sl_entry->refcode == NULL) {
+		log_msg(event, "Memory allocaion failed.\n");
+		return;
+	}
 	strcpy(event->sl_entry->refcode, menu_num_str);
 
-	event->sl_entry->description = (char *)malloc(strlen(msg)+1);
+	event->sl_entry->description = malloc(strlen(msg)+1);
+	if (event->sl_entry->description == NULL) {
+		free(event->sl_entry->refcode);
+		log_msg(event, "Memory allocaion failed.\n");
+		return;
+	}
 	strcpy(event->sl_entry->description, msg);
 
 	dbg("menugoal: number = %d, message = \"%s\"", menu_num, msg);
@@ -503,8 +521,13 @@ process_v6(struct event *event)
 	dbg("Processing version 6 event");
 
 	/* create and populate the servicelog entry */
-	event->sl_entry = (struct sl_event *)malloc(sizeof(struct sl_event));
-	rtas_data = (struct sl_data_rtas *)malloc(sizeof(struct sl_data_rtas));
+	event->sl_entry = malloc(sizeof(struct sl_event));
+	if (event->sl_entry == NULL)
+		goto sl_entry;
+
+	rtas_data = malloc(sizeof(struct sl_data_rtas));
+	if (rtas_data == NULL)
+		goto rtas_data;
 
 	memset(event->sl_entry, 0, sizeof(struct sl_event));
 	memset(rtas_data, 0, sizeof(struct sl_data_rtas));
@@ -519,15 +542,19 @@ process_v6(struct event *event)
 	mt = rtas_get_mt_scn(event->rtas_event);
 	if (mt != NULL) {
 		event->sl_entry->machine_model =
-				(char *)malloc(strlen(mt->mtms.model)+1);
+				malloc(strlen(mt->mtms.model)+1);
 		if (event->sl_entry->machine_model)
 			strcpy(event->sl_entry->machine_model, mt->mtms.model);
+		else
+			goto machine_model;
 
 		event->sl_entry->machine_serial =
-				(char *)malloc(strlen(mt->mtms.serial_no)+1);
+				malloc(strlen(mt->mtms.serial_no)+1);
 		if (event->sl_entry->machine_serial)
 			strcpy(event->sl_entry->machine_serial,
 			       mt->mtms.serial_no);
+		else
+			goto machine_serial;
 	}
 
 	/*
@@ -552,7 +579,9 @@ process_v6(struct event *event)
 	}
 
 	event->sl_entry->raw_data_len = event->length;
-	event->sl_entry->raw_data = (unsigned char *)malloc(event->length);
+	event->sl_entry->raw_data = malloc(event->length);
+	if (event->sl_entry->raw_data == NULL)
+		goto raw_data;
 	memcpy(event->sl_entry->raw_data, event->event_buf, event->length);
 
 	/* populate the "additional data" section of the servicelog entry */
@@ -593,4 +622,19 @@ process_v6(struct event *event)
 		report_menugoal(event, privhdr, usrhdr);
 
 	return 0;
+
+raw_data:
+	if (event->sl_entry->machine_serial)
+		free(event->sl_entry->machine_serial);
+machine_serial:
+	if (event->sl_entry->machine_model)
+		free(event->sl_entry->machine_model);
+machine_model:
+	free(rtas_data);
+rtas_data:
+	free(event->sl_entry);
+sl_entry:
+	log_msg(event, "Memory allocation failed\n");
+	return -1;
+
 }

commit 7281fe95ecdaf0f6767c84450746060d85ab9bd9

rtas_errd: Fix possible NULL pointer dereference
Just return if structure memory allocation fails.
Also remove redundant memory allocation check in free path.

Fixes: d9eddb2e (rtas_errd: Remove cast from malloc() + add error check)
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
CC: Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
Acked-by: Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>

rtas_errd/servicelog.c
@@ -138,7 +138,7 @@ add_callout(struct event *event, char pri, int type, char *proc, char *loc,
 	}
 
 	if (!callout)
-		goto mem_fail;
+		return;
 
 	callout->priority = pri;
 	callout->type = type;
@@ -186,8 +186,8 @@ mem_fail:
 		free(callout->location);
 	if (callout->procedure)
 		free(callout->procedure);
-	if (callout)
-		free(callout);
+	free(callout);
+
 	log_msg(event, "Memory allocation failed");
 	return;
 }
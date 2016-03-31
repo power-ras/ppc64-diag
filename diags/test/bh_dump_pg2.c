acb2cb25a61495ec6209a288d3fcbba8686756b2
Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
2016-03-31 16:44:25 +0530

    diags/tests: Validate sg device before reading diagnostics page
    Validate enclosure device and type before reading diagnostics page.
    
    Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
    Reviewed-by: Ankit Kumar<ankit@linux.vnet.ibm.com>

--- diags/test/bh_dump_pg2.c
+++ diags/test/bh_dump_pg2.c
@@ -23,6 +23,7 @@ main(int argc, char **argv)
 	char *path;
 	int fd;
 	struct bluehawk_diag_page2 dp;
+	struct dev_vpd vpd;
 	int result;
 
 	if (argc != 3) {
@@ -36,18 +37,40 @@ main(int argc, char **argv)
 		exit(2);
 	}
 	snprintf(dev_sg, 20, "/dev/%s", sg);
+
+	/* Validate enclosure device */
+	if (valid_enclosure_device(dev_sg))
+		exit(0);
+
+	/* Read enclosure vpd */
+	memset(&vpd, 0, sizeof(vpd));
+	result = read_vpd_from_lscfg(&vpd, sg);
+	if (vpd.mtm[0] == '\0') {
+		fprintf(stderr,
+			"Unable to find machine type/model for %s\n", sg);
+		exit(1);
+	}
+
+	if (strcmp(vpd.mtm, "5888") && strcmp(vpd.mtm, "EDR1")) {
+		fprintf(stderr,
+			"Not a valid bluehawk enclosure : %s", sg);
+		exit (1);
+	}
+
 	fd = open(dev_sg, O_RDWR);
 	if (fd < 0) {
 		perror(dev_sg);
 		exit(3);
 	}
-	result = get_diagnostic_page(fd, RECEIVE_DIAGNOSTIC, 2,
-		 &dp, sizeof(dp));
+	result = get_diagnostic_page(fd, RECEIVE_DIAGNOSTIC,
+				     2, &dp, sizeof(dp));
 	if (result != 0) {
 		perror("get_diagnostic_page");
 		exit(4);
 	}
+
 	if (write_page2_to_file(path, &dp, sizeof(dp)) != 0)
 		exit(5);
+
 	exit(0);
 }

2891decfd90b1ee904583cefa8284e7375d610c5
Ankit Kumar <ankit@linux.vnet.ibm.com>
2017-06-07 11:39:03 +0530

    Introduces single command to dump enclosure data for all supported enclosure
    Present diagnostics test suite has separate test cases for all enclosure to
    dump diagnostics page to a file.
    This patch merges all different enclosure test case used to dump diagnostics
    page to a file into a single test case.
    
    Signed-off-by: Ankit Kumar <ankit@linux.vnet.ibm.com>
    Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>

--- diags/test/encl_dump_pg2.c
+++ diags/test/encl_dump_pg2.c
@@ -1,39 +1,34 @@
-#include <sys/types.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <errno.h>
 #include <fcntl.h>
-#include <sys/stat.h>
-#include <scsi/sg.h>
 #include <scsi/scsi.h>
-#include <scsi/scsi_ioctl.h>
-#include <sys/ioctl.h>
 
-#include "encl_common.h"
 #include "bluehawk.h"
-#include "test_utils.h"
+#include "homerun.h"
+#include "slider.h"
 
-/* Dump bluehawk enclosure pg2 */
-int
-main(int argc, char **argv)
+/* Dump enclosure pg2 */
+int main(int argc, char **argv)
 {
 	char *sg;
 	char dev_sg[20];
 	char *path;
 	int fd;
-	struct bluehawk_diag_page2 dp;
+	void *dp;
 	struct dev_vpd vpd;
 	int result;
+	int size = 0;
 
 	if (argc != 3) {
 		fprintf(stderr, "usage: %s sgN output_file\n", argv[0]);
 		exit(1);
 	}
+
 	sg = argv[1];
 	path = argv[2];
 	if (strncmp(sg, "sg", 2) != 0 || strlen(sg) > 6) {
-		fprintf(stderr, "bad format of sg arg\n");
+		fprintf(stderr, "bad format of sg argument\n");
 		exit(2);
 	}
 	snprintf(dev_sg, 20, "/dev/%s", sg);
@@ -51,26 +46,43 @@ main(int argc, char **argv)
 		exit(1);
 	}
 
-	if (strcmp(vpd.mtm, "5888") && strcmp(vpd.mtm, "EDR1")) {
-		fprintf(stderr,
-			"Not a valid bluehawk enclosure : %s", sg);
+	if ((!strcmp(vpd.mtm, "5888")) || (!strcmp(vpd.mtm, "EDR1"))) {
+		size = sizeof(struct bluehawk_diag_page2);
+	} else if (!strcmp(vpd.mtm, "5887")) {
+		size = sizeof(struct hr_diag_page2);
+	} else if (!strcmp(vpd.mtm, "ESLL")) {
+		size = sizeof(struct slider_lff_diag_page2);
+	} else if (!strcmp(vpd.mtm, "ESLS")) {
+		size = sizeof(struct slider_sff_diag_page2);
+	} else {
+		fprintf(stderr,"Not a valid enclosure : %s\n", sg);
 		exit (1);
+	}
+
+	dp = (char *)malloc(size);
+	if (!dp) {
+		perror(dp);
+		exit(2);
 	}
 
 	fd = open(dev_sg, O_RDWR);
 	if (fd < 0) {
+		free(dp);
 		perror(dev_sg);
 		exit(3);
 	}
-	result = get_diagnostic_page(fd, RECEIVE_DIAGNOSTIC,
-				     2, &dp, sizeof(dp));
+	result = get_diagnostic_page(fd, RECEIVE_DIAGNOSTIC, 2, dp, size);
 	if (result != 0) {
+		free(dp);
 		perror("get_diagnostic_page");
 		exit(4);
 	}
 
-	if (write_page2_to_file(path, &dp, sizeof(dp)) != 0)
+	if (write_page2_to_file(path, dp, size) != 0) {
+		free(dp);
 		exit(5);
+	}
 
+	free(dp);
 	exit(0);
 }

power-ras/ppc64-diag

commit c77991f8ec0a9d2962db5cec65d2f42909415dbf

ppc64-diag: Fix compiler warning for return value being ignored
Signed-off-by: Shivaprasad G Bhat <sbhat@linux.ibm.com>
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>

opal_errd/opal-elog-parse/opal-elog-parse.c
@@ -143,7 +143,12 @@ int read_elog(char path[], char **buf){
 	int ret = 0;
 	int platform_log_fd = -1;
 
-	chdir(opt_platform_dir);
+	if (chdir(opt_platform_dir) < 0) {
+		fprintf(stderr, "Failed to change to platform log directory"
+				": %s\n", opt_platform_dir);
+		return -1;
+	}
+
 	if (stat(path, &sbuf) == -1){
 		fprintf(stderr, "Error accessing %s\n",path);
 		return -1;

ela/catalogs.cpp
@@ -930,7 +930,11 @@ MatchVariant::compute_regex_text(void)
 		goto free_mem;
 	}
 
-	fgets(regex_cstr,REGEX_MAXLEN, in);
+	if (fgets(regex_cstr, REGEX_MAXLEN, in) == NULL) {
+		parent->parser->semantic_error("cannot read regex text");
+		spclose(in, cpid);
+		goto free_mem;
+	}
 
 	spclose(in, cpid);
 

opal_errd/opal_errd.c
@@ -374,16 +374,20 @@ static void rotate_logs(const char *elog_dir, int max_logs, int elog_type)
 	else
 		filter = &is_informational_elog_file;
 
-        /* Retrieve file list */
-        chdir(elog_dir);
-        nfiles = scandir(elog_dir, &filelist, filter, alphasort);
-        if (nfiles < 0) {
+	/* Retrieve file list */
+	if (chdir(elog_dir) < 0) {
+		syslog(LOG_NOTICE, "chdir failed\n");
+		return;
+	}
+
+	nfiles = scandir(elog_dir, &filelist, filter, alphasort);
+	if (nfiles < 0) {
 		syslog(LOG_NOTICE, "Error scanning the log directory %s\n",
 					elog_dir);
 		return;
 	}
 
-        for (i = 0; i < nfiles; i++) {
+	for (i = 0; i < nfiles; i++) {
 		if (!trim){
 			free(filelist[i]);
 			continue;
@@ -1072,8 +1076,14 @@ int main(int argc, char *argv[])
 			 * event, we'll just scan the directory anyway
 			 */
 			rc = poll(fds, sizeof(fds)/sizeof(struct pollfd), POLL_TIMEOUT);
-			if (rc > 0 && fds[INOTIFY_FD].revents)
-				read(fds[INOTIFY_FD].fd, inotifybuf, sizeof(inotifybuf));
+			if (rc > 0 && fds[INOTIFY_FD].revents) {
+				if (read(fds[INOTIFY_FD].fd, inotifybuf, sizeof(inotifybuf)) == -1) {
+					syslog(LOG_WARNING, "Can not read platform log directory:"
+					       " (%d:%s)\n", errno, strerror(errno));
+					goto exit;
+				}
+			}
+
 			if (rc > 0 && fds[UDEV_FD].revents) {
 				udev_dev = udev_monitor_receive_device(udev_mon);
 				devpath = udev_device_get_devpath(udev_dev);

rtas_errd/prrn.c
@@ -244,8 +244,7 @@ static int add_drconf_phandles()
 		return -1;
 	}
 
-	(void)fread(membuf, sbuf.st_size, 1, fd);
-	if (ferror(fd)) {
+	if ((fread(membuf, sbuf.st_size, 1, fd) < sbuf.st_size) && ferror(fd)) {
 		fclose(fd);
 		free(membuf);
 		return -1;

rtas_errd/rtas_errd.c
@@ -86,7 +86,8 @@ daemonize(void)
 		goto daemonize_error;
 
 	/* change to a safe dir */
-	chdir("/");
+	if (chdir("/") < 0)
+		goto daemonize_error;
 
 	/* clear file mode mask */
 	umask(0);

commit 226bc954adb64e4e34b4682ada2979d931fb28b1

opal-elog-parse: Skip change of directory when file is given
c77991f8 started to handle the return values for all the system calls.
The chdir earlier called upon when the file path itself given used to pass
as the failure from that was not handled. With the mentioned commit, the
read_elog began to return failure when chdir failed, resulting in command
failure when -f is given.

This patch skips the chdir when the path is given by the user.

Fixes: c77991f8 (Fix compiler warning for return value being ignored)
Signed-off-by: Shivaprasad G Bhat <sbhat@linux.ibm.com>
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>

opal_errd/opal-elog-parse/opal-elog-parse.c
@@ -134,8 +134,8 @@ char *get_elog_filename_str(const char *eid_str)
 	return get_elog_filename_int(eid);
 }
 
-int read_elog(char path[], char **buf){
-
+int read_elog(char path[], char **buf, bool skip_chdir)
+{
 	struct stat sbuf;
 	size_t bufsz;
 	ssize_t readsz = 0;
@@ -143,7 +143,7 @@ int read_elog(char path[], char **buf){
 	int ret = 0;
 	int platform_log_fd = -1;
 
-	if (chdir(opt_platform_dir) < 0) {
+	if (!skip_chdir && (chdir(opt_platform_dir) < 0)) {
 		fprintf(stderr, "Failed to change to platform log directory"
 				": %s\n", opt_platform_dir);
 		return -1;
@@ -242,7 +242,7 @@ int elogdisplayfile(char *elog_path, uint32_t eid, int display_all)
 	ssize_t sz = 0;
 	int offset = ELOG_ID_OFFSET;
 
-	sz = read_elog(elog_path, &buffer);
+	sz = read_elog(elog_path, &buffer, true);
 	if(sz < 0) {
 		return -1;
 	/* Make sure we read minimum data needed in this function */
@@ -298,7 +298,7 @@ int elogdisplayentry(uint32_t eid, int display_all)
 			continue;
 		}
 
-		sz = read_elog(filelist[i]->d_name, &buffer);
+		sz = read_elog(filelist[i]->d_name, &buffer, false);
 
 		if(sz < 0) {
 			free(filelist[i]);
@@ -341,7 +341,7 @@ int elog_summary(char *elog_path, uint32_t service_flag)
 	printf("|ID       Date       Time     SRC        Creator           Event Severity      |\n");
 	printf("|------------------------------------------------------------------------------|\n");
 
-	sz = read_elog(elog_path, &buffer);
+	sz = read_elog(elog_path, &buffer, true);
 	if (sz < 0)
 		return -1;
 
@@ -390,7 +390,7 @@ int eloglist(uint32_t service_flag)
 	}
 
 	for (i = 0; i < nfiles; i++){
-		sz = read_elog(filelist[i]->d_name, &buffer);
+		sz = read_elog(filelist[i]->d_name, &buffer, false);
 		if (sz < 0){
 			free(filelist[i]);
 			continue;